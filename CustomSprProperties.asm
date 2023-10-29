dd54a3b25baedf4abedb02af7aa1a8a7ec062503
Jared_Brian_ <jaredbrian5828@gmail.com>
2023-10-29 00:33:27 -0600

    reduced blob damage

--- CustomSprProperties.asm
+++ CustomSprProperties.asm
@@ -3,3 +3,6 @@ db $43 ; set it unalive with hitbox3 (default property)
 
 org $0DB44C+$EE ; castle mantle
 db $48 ; set it unalive with hitbox8 (default property)
+
+org $0DB266+$8F ; Blob bump damage
+db $03 ; Original: 05
