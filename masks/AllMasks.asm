d3189e78aa5723c49d427cc8349d6008e567a7a3
scawful <justin.scofield@outlook.com>
2023-11-02 22:31:13 -0400

    Add StalfosMask Jump

--- masks/AllMasks.asm
+++ masks/AllMasks.asm
@@ -66,6 +66,52 @@ UpdateFierceDeityLinkPalette:
   dw #$7BDE, #$7FFF, #$6318, #$3A14, #$4EDA, #$14A5, #$4629, #$1078
   dw #$3125, #$49A7, #$39CE, #$56ED, #$6350, #$1571, #$4A52, #$0F5F
 }
+
+NewBookCode:
+{
+  JSL $07983A ; Reset swim state
+  LDA $46 : BNE .cantuseit
+  LDA #$02 : STA $5D ; state recoil
+  LDA #$01 : STA $4D ; state recoil 2
+
+  ; Length of the jump
+  LDA #$20 
+
+  STA $46 
+
+  ; Height of the jump
+  LDA #$24 
+
+  ; Set vertical resistance 
+  STA $29
+  STA $02C7
+  ; Set Links direction to right(?)
+  LDA #$08 : STA $0340 : STA $67
+
+  ; Reset Link movement offsets 
+  STZ $31
+  STZ $30
+
+  LDA $F4 : AND #$08 : BEQ .noUp
+      LDA #-8 ; Change that -8 if you want higher speed moving up
+      STA $27 ; Vertical recoil
+  .noUp
+  LDA $F4 : AND #$04 : BEQ .noDown
+      LDA #8  ; Change that -8 if you want higher speed moving down
+      STA $27
+  .noDown
+  LDA $F4 : AND #$02 : BEQ .noLeft
+      LDA #-8 ; Change that -8 if you want higher speed moving left
+      STA $28 ; Horizontal recoil
+  .noLeft
+  LDA $F4 : AND #$01 : BEQ .noRight
+      LDA #8  ; Change that 8 if you want higher speed moving right
+      STA $28
+  .noRight
+  .cantuseit
+  RTL
+}
+
 pushpc
   
 ; =============================================================================
@@ -113,6 +159,16 @@ LinkItem_Quake:
   %ResetToLinkGraphics()
 
 .return
+
+  LDA $02B2 : CMP #$02 : BNE .done
+  BIT $3A : BVS .done ;if Y or B are already pressed
+  LDA $6C : BNE .done ; if we are standing in a dooray or not
+
+  ; Link_CheckNewY_ButtonPress
+  JSR $B073 : BCC .done ; Check if we just pressed Y Button  
+  JSL NewBookCode
+.done
+
   CLC 
   RTS
 }
