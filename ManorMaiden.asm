1274c3c351f8f685c5d644c838d4f9aab3dfcb89
scawful <justin.scofield@outlook.com>
2023-11-04 01:40:30 -0400

    Add ManorMaiden experimental

---
+++ ManorMaiden.asm
@@ -0,0 +1,28 @@
+; Jeibumpkin ? Room 7C
+; Dialoge ID - 011D
+
+org $09A1EC
+  JSL CheckForMaidenInLibrary
+
+pullpc 
+
+CheckForMaidenInLibrary:
+{
+  LDA $A0 : CMP.b #$7C : BNE .notTheLibrary
+
+  LDA $7FF9D2 : BNE .dialogue_played
+
+    LDA #$1D : LDY #$01
+    JSL Sprite_ShowMessageUnconditional
+    LDA #$01 : STA $7FF9D2
+
+    .dialogue_played
+
+  .notTheLibrary
+  ; Check for blind room vanilla
+  REP #$20
+  LDA.b $A0
+  RTL
+}
+
+pushpc
